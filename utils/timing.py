efc3415fc0d1472f18a8da7026c1db40238e26f4
Steve Farrell <sfarrell@cern.ch>
2016-09-06 11:46:42 -0700

    Added code for computing thread-avg thruput.
    Two new functions in utils.timing. The first one computes the event loop
    time averaged over threads, and the other computes the throughput using
    this new event loop time calculation. This throughput calculation should
    be more stable for fluctuations due to idle cores at the end of the
    event loop, since the idle time is ignored in this case.

--- utils/timing.py
+++ utils/timing.py
@@ -22,6 +22,19 @@ def get_evloop_time(job):
     """Duration of the event loop in seconds"""
     return (get_evloop_end_time(job) - get_evloop_start_time(job))*1e-9
 
+def get_avg_thread_time(job):
+    """Get the thread-averaged event loop time in seconds"""
+    tids = job.timeline_results['tids']
+    unique_tids = np.unique(tids)
+    start = get_evloop_start_time(job)
+    ends = job.timeline_results['ends']
+    time = 0
+    for tid in unique_tids:
+        thread_ends = ends[tids == tid]
+        thread_end = thread_ends[-1]
+        time += (thread_end - start)*1e-9
+    return time / job.nThread
+
 def get_initialization_time(job):
     """Duration of job initialization in seconds"""
     events_start = get_evloop_start_time(job)
@@ -48,6 +61,14 @@ def get_throughput(job):
     ignoring initialization and finalization.
     """
     return job.nEvent / get_evloop_time(job)
+
+def get_avg_throughput(job):
+    """
+    Calculates thread-averaged event throughput (events/s) for a job.
+    Differs from get_throughput method above by ignoring idle time of threads
+    at the end of the event loop, resulting in a more stable measurement.
+    """
+    return job.nEvent / get_avg_thread_time(job)
 
 def calc_alg_timings(job):
     """
